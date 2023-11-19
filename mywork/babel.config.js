746b008ade82a6281041c47cec12eb7ec532350b
hvinci <1211233831@qq.com>
2023-11-19 15:12:09 +0800

    update jest work

--- mywork/babel.config.js
+++ mywork/babel.config.js
@@ -2,8 +2,14 @@
  * @Author: hvinci
  * @Date: 2023-11-18 05:21:46
  * @LastEditors: hvinci
- * @LastEditTime: 2023-11-19 00:58:37
+ * @LastEditTime: 2023-11-19 13:42:58
  * @Description: 
  * 
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
  */
+module.exports = {
+  presets: [
+ ['@babel/preset-env', {targets: {node: 'current'}}],
+   '@babel/preset-typescript',
+ ],
+};
